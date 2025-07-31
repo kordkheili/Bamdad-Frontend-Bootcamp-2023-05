b501b326e6b3c8e433cd426eeee2901337ded6f3
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2025-07-31 10:30:36 +0330

    images added

--- challenges/3-js/js-challenge03-password-strength/starter/script.js
+++ challenges/3-js/js-challenge03-password-strength/starter/script.js
@@ -1,20 +1,2 @@
 // گرفتن ورودی پسورد از کاربر
 const password = prompt("لطفاً پسورد خود را وارد کنید:");
-
-// المان نمایش نتیجه
-const resultDiv = document.getElementById("result");
-
-// بررسی قدرت پسورد و تنظیم رنگ و متن
-if (password && password.length < 6) {
-  resultDiv.textContent = "ضعیف";
-  resultDiv.className = "weak";
-} else if (password && password.length <= 10) {
-  resultDiv.textContent = "متوسط";
-  resultDiv.className = "medium";
-} else if (password && password.length > 10) {
-  resultDiv.textContent = "قوی";
-  resultDiv.className = "strong";
-} else {
-  resultDiv.textContent = "پسورد وارد نشد";
-  resultDiv.style.color = "#666";
-}
